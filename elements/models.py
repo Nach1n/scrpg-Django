3f41bbfe173717160c8d4d8c023aeefff6b30663
liapino1 <l.ancavilpno@gmail.com>
2019-09-16 16:42:02 -0300

    Save titles and images in BD

--- elements/models.py
+++ elements/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+   title = models.CharField(max_length=255)
+
+   def __str__(self):
+       return self.title
+
+class Imgs(models.Model):
+    imgs = models.FileField(upload_to='minuta/', blank=True, null=True)
+
+    def __str__(self):
+        return str(self.imgs)
+
--- elements/views.py
+++ elements/views.py
@@ -1,23 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
+from .models import Page, Imgs
 
-def scrape():
+def scrape(request):
     """ Returns all the titles of the publications in a list. Page: https://www.applesfera.com/"""
+    template = 'index.html'
     list_titles = []
+    list_imgs = []
     html_doc = requests.get('https://www.applesfera.com/', headers={'Accept-Encoding':'identity'})
     soup = BeautifulSoup(html_doc.content, "html.parser")
     publication = soup.find_all('div',{'class':'abstract-content'})
-    print(len(publication)) #Return large of de the list publicaction
+    imgs = soup.find_all('div',{'class':'base-asset-image'})
     
     for i in publication:
         title = i.find('a').getText()
         list_titles.append(title)
-        
-    for x in list_titles:
-        print(x)
+    
+    for x in imgs:
+        images_final = x.find('img').attrs['src']
+        list_imgs.append(images_final)
 
-scrape()
+    """ Save elements in BD """
+    for i in list_titles:
+        Page.objects.create(title=i)
+
+    for j in list_imgs:
+        Imgs.objects.create(imgs=j)
+
+    return render(request, template, {})
+
 
 
 
